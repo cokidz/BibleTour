d6551e5688a4e359d79036740d4d40b3d45687db
cokidz <wondol16@gmail.com>
2025-04-03 12:48:09 +0900

    Add files via upload

---
+++ books.js
@@ -0,0 +1,24 @@
+export const books = {
+    "마가복음": {
+        name: "마가복음",
+        chapters: 16,
+        image: "mark.jpg",
+        path: [
+            { chapter: 1, x: 50, y: 350 }, { chapter: 2, x: 100, y: 340 }, { chapter: 3, x: 150, y: 330 },
+            { chapter: 4, x: 200, y: 320 }, { chapter: 5, x: 250, y: 310 }, { chapter: 6, x: 300, y: 300 },
+            { chapter: 7, x: 350, y: 290 }, { chapter: 8, x: 400, y: 280 }, { chapter: 9, x: 450, y: 270 },
+            { chapter: 10, x: 500, y: 260 }, { chapter: 11, x: 550, y: 250 }, { chapter: 12, x: 600, y: 240 },
+            { chapter: 13, x: 650, y: 230 }, { chapter: 14, x: 700, y: 220 }, { chapter: 15, x: 750, y: 210 },
+            { chapter: 16, x: 780, y: 200 }
+        ]
+    },
+    "요한복음": { // 새 지도 예시
+        name: "요한복음",
+        chapters: 21,
+        image: "john.jpg",
+        path: [
+            { chapter: 1, x: 50, y: 350 }, { chapter: 2, x: 100, y: 340 }, /* ... */ { chapter: 21, x: 780, y: 200 }
+        ]
+    }
+    // 추가 지도 계속 넣기
+};
